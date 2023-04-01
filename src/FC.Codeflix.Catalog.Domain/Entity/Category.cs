8cf52a705e91784af09a3bad8abc6c5a805a6b93
Victor A Custodio <victoralvescustodio@gmail.com>
2023-04-01 02:45:15 -0400

    :tada: Commit inicial

---
+++ src/FC.Codeflix.Catalog.Domain/Entity/Category.cs
@@ -0,0 +1,27 @@
+﻿namespace FC.Codeflix.Catalog.Domain.Entity;
+public class Category
+{
+    public Guid Id { get; private set; }
+    public String Name { get; private set; }
+    public String Description { get; private set; }
+    public Boolean IsActive { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public Category(string name, string description, bool isActive = true)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Description = description;
+        IsActive = isActive;
+        CreatedAt = DateTime.Now;
+    }
+
+    Validate();
+
+    public Validate()
+    {
+        if (String.IsNullOrWhiteSpace)
+        {
+            throw new EntityValidationException($"{nameof(Name)} should not be empty or null");
+        }
+    }
+}
